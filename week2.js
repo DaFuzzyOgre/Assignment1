6114797f41774f946d139b260170da502434908f
Carleigh Feldhouse <you@example.com>
2022-02-01 22:48:09 -0500

    pt5

--- week2.js
+++ week2.js
@@ -28,7 +28,7 @@ app.get("/project", function(req,res){
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
 
-app.get('/week2', function(req, res){    
+app.get('/home', function(req, res){    
   res.render('index',{user:"John Smith"})
 });
 
